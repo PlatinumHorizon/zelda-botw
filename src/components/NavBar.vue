309a835648916350c45d96efe4275341daf9da00
Gabriel F. Xavier <gfzxavier@outlook.com>
2023-01-20 16:56:08 -0300

    refactor: rework navbar design

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,26 @@
 <template>
-  <div id="nav-bar">
-    <div class="absolute bg-sheikah-white bg-sheikah-pattern-size w-full h-[135px] top-12 border-y-[3px] border-golden-50">
-      <img src="../assets/img/logo.png" class="absolute top-[-47px] max-h-[202px] ml-6" alt="">
+  <div id="nav-bar" class="ml-10 mr-32 mt-8">
+    <div class="flex justify-between items-center">
+      <div class="flex gap-10">
+        <div v-for="item in items" :key="item.title" class="flex items-center gap-1 cursor-pointer transition-all duration-500 hover:gap-5 hover:drop-shadow-blue-glow">
+          <div>
+            <img src="../assets/svg/left-arrow.svg" class="h-2">
+          </div>
+          <div class="font-hylian text-3xl text-white">
+            {{ item.title }}
+          </div>
+          <div>
+            <img src="../assets/svg/right-arrow.svg" class="h-2">
+          </div>
+        </div>
+      </div>
+      <div class="flex gap-6">
+        <div v-for="socialMedia in contact" :key="socialMedia.vector" class="transition-all duration-500 hover:drop-shadow-blue-glow hover:translate-y-[-3px]">
+          <a :href="socialMedia.link" target="_blank">
+            <img :src="socialMedia.vector" class=" h-7">
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -12,9 +31,41 @@ export default {
   data() {
     return {
 			items: [
-				{}
-			]
+				{
+          title:'Home',
+          anchorLocation: '#home'
+        },
+        {
+          title:'Story',
+          anchorLocation: '#home'
+        },
+        {
+          title:'Wallpapers',
+          anchorLocation: '#home'
+        },
+			],
+      contact: [
+        {
+          vector: require('../assets/svg/linkedin.svg'),
+          link: 'https://www.linkedin.com/in/gabriel-fernandez-xavier-2758aa185/'
+        },
+        {
+          vector: require('../assets/svg/facebook.svg'),
+          link: 'https://www.facebook.com/gabriel.fernandez.90857901'
+        },
+        {
+          vector: require('../assets/svg/instagram.svg'),
+          link: 'https://www.instagram.com/gfzxavier/'
+        },
+        {
+          vector: require('../assets/svg/youtube.svg'),
+          link: 'https://www.youtube.com/channel/UC7CakzAyIR3TSisNGRUiP1A'
+        }
+      ]
     }
+  },
+  methods: {
+    
   }
 }
 </script>
